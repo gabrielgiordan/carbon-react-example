c1b2aad0e311bf512d74c41a95776bda2a7813af
gabrielgiordan <gabrielgiordan@users.noreply.github.com>
2020-06-06 03:00:07 +0000

    deploy: 1210226167233e27ec3784f4859b90eaa6e92989

---
+++ precache-manifest.69675781087531f42965b2f96de53430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f647b1a2baa4f9b702f4a1449d2c52",
+    "url": "https://gabrielgiordan.github.io/carbon-react-example/index.html"
+  },
+  {
+    "revision": "42ac097dbdadb8713034",
+    "url": "https://gabrielgiordan.github.io/carbon-react-example/static/css/main.3ad8cb2c.chunk.css"
+  },
+  {
+    "revision": "b9e78308f8e636bf8541",
+    "url": "https://gabrielgiordan.github.io/carbon-react-example/static/js/2.b2096503.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "https://gabrielgiordan.github.io/carbon-react-example/static/js/2.b2096503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ac097dbdadb8713034",
+    "url": "https://gabrielgiordan.github.io/carbon-react-example/static/js/main.0240119f.chunk.js"
+  },
+  {
+    "revision": "d52dbe26bec2ab4013f4",
+    "url": "https://gabrielgiordan.github.io/carbon-react-example/static/js/runtime-main.af9d7154.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://gabrielgiordan.github.io/carbon-react-example/precache-manifest.f9392a92ac7c5baceebdbe7942132674.js"
+  "https://gabrielgiordan.github.io/carbon-react-example/precache-manifest.69675781087531f42965b2f96de53430.js"
 );
 
 self.addEventListener('message', (event) => {

9b4f8e98eab63a9085ca4f8df53eb466ab800e4f
gabrielgiordan <gabrielgiordan@users.noreply.github.com>
2020-06-06 00:19:21 +0000

    deploy: ba8442b0574515adb129c516d7d2adf8d2fa9ce6

---
+++ precache-manifest.56f776bac43fe438771ad850026fad88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2329a47630137cdcb598732c6a5b5fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "292163195212ff5bfcb7",
+    "url": "/static/css/main.3ad8cb2c.chunk.css"
+  },
+  {
+    "revision": "7e4b5f8aefea86a06e7f",
+    "url": "/static/js/2.70f24cb5.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/static/js/2.70f24cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292163195212ff5bfcb7",
+    "url": "/static/js/main.a082b4b6.chunk.js"
+  },
+  {
+    "revision": "a9a3fc9f9d34e93c4542",
+    "url": "/static/js/runtime-main.715c942b.js"
+  }
+]);

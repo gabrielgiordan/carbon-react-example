3aa23c23b4fd50b5b0289303f40ae523f361d1e9
Gabriel Medina <hello@gabrielgiordano.com>
2020-06-06 02:00:27 -0300

    Fix app test not wrapped in act

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>,
-    div,
-  );
+  act(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </React.StrictMode>,
+      div,
+    );
+  });
+
   ReactDOM.unmountComponentAtNode(div);
 });

604136989152ca28c8e4bad3dc3106651cdaf329
gabrielgiordan <gabrielgiordan@users.noreply.github.com>
2020-07-02 13:54:59 +0000

    deploy: ebc2e56d213fadf9806eb03915372908188005e8

---
+++ precache-manifest.858fcbc23b0b080ddb5c343976685aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd65ff18c32ae23c3d6945c1c1acca9e",
+    "url": "https://carbon.gabrielgiordano.com/index.html"
+  },
+  {
+    "revision": "ff2d017a8b1cdd2a1367",
+    "url": "https://carbon.gabrielgiordano.com/static/css/main.4b56ff85.chunk.css"
+  },
+  {
+    "revision": "8523d1fc2c5bb8b4b110",
+    "url": "https://carbon.gabrielgiordano.com/static/js/2.29d9627b.chunk.js"
+  },
+  {
+    "revision": "989fc6b0d48d15ee50fba08f57250570",
+    "url": "https://carbon.gabrielgiordano.com/static/js/2.29d9627b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2d017a8b1cdd2a1367",
+    "url": "https://carbon.gabrielgiordano.com/static/js/main.2e73955f.chunk.js"
+  },
+  {
+    "revision": "038ff8d038e59d13abb5",
+    "url": "https://carbon.gabrielgiordano.com/static/js/runtime-main.f79f3708.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://carbon.gabrielgiordano.com/precache-manifest.573ecf19c3e2148403b1577c2ef0da24.js"
+  "https://carbon.gabrielgiordano.com/precache-manifest.858fcbc23b0b080ddb5c343976685aa9.js"
 );
 
 self.addEventListener('message', (event) => {
